60036af1a69a0ca88fbb23be255a4912542764ca
kylekatarnls <kylekatarnls@gmail.com>
2023-05-03 14:17:24 +0200

    Add sum test

--- tests/TransformationTest.php
+++ tests/TransformationTest.php
@@ -8,66 +8,66 @@ test('Transformation', function () {
             yield $i;
         }
     };
-//
-//    expect(
-//        Transformation::take($gen())
-//            ->filter(static fn (int $number) => $number !== 4)
-//            ->map(static fn (int $number) => [
-//                'number' => $number,
-//                'odd' => $number % 2 === 0,
-//            ])
-//            ->filter(key: 'odd')
-//            ->values()
-//            ->array()
-//            ->get(),
-//    )->toBe(
-//        [
-//            [
-//                'number' => 2,
-//                'odd' => true,
-//            ],
-//            [
-//                'number' => 6,
-//                'odd' => true,
-//            ],
-//        ],
-//    );
-//
-//    expect(
-//        Transformation::take(42)->get(),
-//    )->toBe(
-//        42,
-//    );
-//
-//    expect(
-//        Transformation::take('abc')->then('strtoupper')->get(),
-//    )->toBe(
-//        'ABC',
-//    );
-//
-//    expect(
-//        Transformation::take($gen())->reverse()->get(),
-//    )->toBe(
-//        [6, 5, 4, 3, 2, 1],
-//    );
-//
-//    expect(
-//        Transformation::take([1, 2, 3])->reverse()->get(),
-//    )->toBe(
-//        [3, 2, 1],
-//    );
 
-//    expect(
-//        Transformation::take([1, 2, 3])->first()->get(),
-//    )->toBe(
-//        1,
-//    );
-//
-//    expect(
-//        Transformation::take([1, 2, 3])->last()->get(),
-//    )->toBe(
-//        3,
-//    );
+    expect(
+        Transformation::take($gen())
+            ->filter(static fn (int $number) => $number !== 4)
+            ->map(static fn (int $number) => [
+                'number' => $number,
+                'odd' => $number % 2 === 0,
+            ])
+            ->filter(key: 'odd')
+            ->values()
+            ->array()
+            ->get(),
+    )->toBe(
+        [
+            [
+                'number' => 2,
+                'odd' => true,
+            ],
+            [
+                'number' => 6,
+                'odd' => true,
+            ],
+        ],
+    );
+
+    expect(
+        Transformation::take(42)->get(),
+    )->toBe(
+        42,
+    );
+
+    expect(
+        Transformation::take('abc')->then('strtoupper')->get(),
+    )->toBe(
+        'ABC',
+    );
+
+    expect(
+        Transformation::take($gen())->reverse()->get(),
+    )->toBe(
+        [6, 5, 4, 3, 2, 1],
+    );
+
+    expect(
+        Transformation::take([1, 2, 3])->reverse()->get(),
+    )->toBe(
+        [3, 2, 1],
+    );
+
+    expect(
+        Transformation::take([1, 2, 3])->first()->get(),
+    )->toBe(
+        1,
+    );
+
+    expect(
+        Transformation::take([1, 2, 3])->last()->get(),
+    )->toBe(
+        3,
+    );
 
     $newGen = Transformation::take($gen())->flip()->get();
     expect($newGen)->toBeInstanceOf(Generator::class);
@@ -108,6 +108,18 @@ test('Transformation', function () {
         Transformation::take([2, 4, 6])->count()->get(),
     )->toBe(
         3,
+    );
+
+    expect(
+        Transformation::take([2, 4, 6])->sum()->get(),
+    )->toBe(
+        12,
+    );
+
+    expect(
+        Transformation::take($gen())->sum()->get(),
+    )->toBe(
+        21,
     );
 
     expect(
